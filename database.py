c1d0d44822600bb704fbd667f0858b93cd340f42
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2017-02-28 19:49:38 -0500

    Change tabs to spaces

--- database.py
+++ database.py
@@ -2,14 +2,14 @@ import pymysql
 from pymysql.err import InternalError
 
 class Database:
-	"""Class for mySQL databases
+    """Class for mySQL databases
 
-	Provide a connection and cursor for all pymysql calls.
-	Based on code by Ryan Elizabeth Mitchell.
+    Provide a connection and cursor for all pymysql calls.
+    Based on code by Ryan Elizabeth Mitchell.
 
-	"""
+    """
 
-	def __init__(self):
-		self.conn = pymysql.connect(host='127.0.0.1', unix_socket='/opt/lampp/var/mysql/mysql.sock', user='root', passwd='', charset='utf8')
-		self.cur = self.conn.cursor(pymysql.cursors.DictCursor)
-		self.cur.execute("USE coursescraping")
+    def __init__(self):
+        self.conn = pymysql.connect(host='127.0.0.1', unix_socket='/opt/lampp/var/mysql/mysql.sock', user='root', passwd='', charset='utf8')
+        self.cur = self.conn.cursor(pymysql.cursors.DictCursor)
+        self.cur.execute("USE coursescraping")
